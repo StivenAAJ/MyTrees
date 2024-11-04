08a796bfd0d788a52532fdc388ff4adf368231a7
Stiven Aurelio Arguello Jimenez <stivenjim2005@gmail.com>
2024-11-03 21:51:12 -0600

    Day #7
    This window is responsible for visually representing the log or changes to be made.

---
+++ bitacora.php
@@ -0,0 +1,43 @@
+<?php
+require_once 'utils/functions.php';
+require_once 'utils/trees-funcs.php';
+
+// Verificación de usuario y especies
+session_start();
+if ($_SESSION['user_role'] !== 'ADMIN') {
+    header('Location: warning.php');
+    exit;
+}
+//llamado a la funcion get Trees en funciones para los arboles.
+$trees = getTrees();
+
+require('inc/header.php'); 
+?>
+<h1>Registrar Actualización de Árbol</h1>
+    
+<form action="utils/bita-funcs.php" method="POST" id="bitacora-form">
+        <label for="tree-id">Árbol:</label>
+        <select id="tree-id" name="tree_id" required>
+            <option value="">Seleccione un árbol</option>
+            <?php foreach ($trees as $tree): ?>
+                <option value="<?= $tree['id'] ?>">
+                    <?= "ID: {$tree['id']} - Especie: {$tree['especie']}" ?>
+                </option>
+            <?php endforeach; ?>
+        </select>
+
+        <label for="tam">Tamaño Actual:</label>
+        <input type="text" id="tam" name="tam" required>
+
+        <label for="estado">Estado:</label>
+        <select id="estado" name="estado" required>
+            <option value="DISPONIBLE">DISPONIBLE</option>
+            <option value="VENDIDO">VENDIDO</option>
+        </select>
+
+        <button type="submit" name="update_bitacora">Registrar Actualización</button>
+    </form>
+
+
+
+<?php require('inc/footer.php'); ?>
